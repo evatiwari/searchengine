59f9d316b54b11c61fb33f1718ba60a76ff68272
Eva <eva.tiwari@gmail.com>
2020-06-23 23:11:26 +0530

    Integrated extractive summarizer with the application.

--- search/functions.py
+++ search/functions.py
@@ -1,7 +1,9 @@
 from .models import Result
+from .extract import do_summary
 def handle_uploaded_file(f):  
     server_path='D:/Documents/Server/'
-    result=Result.objects.create(title=f.name,summary='',destination=server_path+f.name)
+    summary=do_summary(server_path+f.name)
+    result=Result.objects.create(title=f.name,summary=summary,destination=server_path+f.name)
     result.save()
     with open(server_path+f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
